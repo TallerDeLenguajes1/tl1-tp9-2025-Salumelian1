75543b95b412472ecf159d1941625ced5b48c7ab
Salustiano <salustianomeliann@gmail.com>
2025-06-10 12:08:56 -0300

    primer commit

---
+++ LectorDirectorio/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using System;
+
+class Program {
+    static void main()
+    {
+        string? path;
+        string[] carpetas; 
+        do
+        {
+            Console.WriteLine("Ingresar el path deseado: ");
+            path = Console.ReadLine();
+            if (!Directory.Exists(path))
+            {
+                Console.WriteLine("EL directorio no existe. ");
+            }
+        } while (!Directory.Exists(path));
+        carpetas = Directory.GetDirectories(path);
+    }
+}
