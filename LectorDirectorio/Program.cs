f4ddc9698ed3e5caecaa252cfeef02482d1de70e
Salustiano <salustianomeliann@gmail.com>
2025-06-10 12:57:05 -0300

    Se termino el punto 1

--- LectorDirectorio/Program.cs
+++ LectorDirectorio/Program.cs
@@ -5,7 +5,7 @@ class Program {
     static void Main(string[] args)
     {
         string? path;
-        string[] carpetas; 
+        string[] carpetas, files;
         do
         {
             Console.WriteLine("Ingresar el path deseado: ");
@@ -16,5 +16,29 @@ class Program {
             }
         } while (!Directory.Exists(path));
         carpetas = Directory.GetDirectories(path);
+        Console.WriteLine("---------Directorios dentro del directorio-----");
+        foreach (string Carpetas in carpetas)
+        {
+            Console.WriteLine(Carpetas);
+        }
+        files = Directory.GetFiles(path);
+        Console.WriteLine("---------Archivos dentro del directorio-----");
+        foreach (string File in files)
+        {
+            Console.WriteLine(File);
+        }
+        string NombreArchivo = "reporte_archivos.csv";
+        string ruta = Path.Combine(path, NombreArchivo);
+        using (StreamWriter escritor = new StreamWriter(ruta, false))
+        {
+            foreach (string archivos in files)
+            {
+                FileInfo info = new FileInfo(archivos);
+                string nombre = info.Name;
+                long Tamaño = info.Length / 1024;
+                DateTime fecha = info.LastWriteTime;
+                escritor.WriteLine($"{nombre}; {Tamaño}; {fecha}");
+            }
+        }
     }
 }
